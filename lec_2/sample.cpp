9f5f8f2bac2f5aa8234f823744ce27259c2d69ac
aashwingarg7679 <aashwingarg7679@gmail.com>
2023-10-17 16:11:11 +0530

    lec 2 done

---
+++ lec_2/sample.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"namaste dunia";
+    cout<<"\n";
+
+    int a =123;
+    cout<<a;
+    cout<<"\n";
+
+
+    char b='v';
+    cout<<b;
+    cout<<"\n";
+
+
+    bool bl=true;
+    cout<<bl<<endl;
+
+
+    float f=1.2;
+    cout<<f<<endl;
+
+
+    double d=1.23;
+    cout<<d<<endl;
+
+    int size=sizeof(a);
+    cout<<"size of a is: "<<size<<endl;
+
+
+
+    int sized=sizeof(d);
+    cout<<"size of d is: "<<sized<<endl;
+}
